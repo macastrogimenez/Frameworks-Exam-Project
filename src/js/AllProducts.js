e0acf3f675589c1874a880ac0d44293a7519fb0d
macastrogimenez <migca@itu.dk>
2026-02-24 23:51:49 +0100

    merged Sine's branch and started with ProductDetail

--- src/js/AllProducts.js
+++ src/js/AllProducts.js
@@ -35,143 +35,7 @@ categories.innerHTML += `
     </div>
   `;
 
-const products = [
-  {
-    name: "Classic Black Tee",
-    color: "Black",
-    color: "Women",
-    image: "../images/tshirt1.png",
-    detailsPage: "ClassicBlackTee.html",
-    description:
-      "A timeless black tee that pairs well with any outfit. Made from soft, breathable cotton for all-day comfort.",
-  },
-  {
-    name: "Flamin' Hot Red Tee",
-    color: "Red",
-    gender: "Women",
-    image: "../images/tshirt2.png",
-    detailsPage: "FlaminHotRedTee.html",
-    description:
-      "Turn up the heat with our Flamin' Hot Red Tee! This vibrant red shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a bold pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.",
-  },
-  {
-    name: "Ocean Blue Tee",
-    color: "Blue",
-    gender: "Women",
-    image: "../images/tshirt3.png",
-    detailsPage: "OceanBlueTee.html",
-    description:
-      "Dive into style with our Ocean Blue Tee! This vibrant blue shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a refreshing pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.",
-  },
-  {
-    name: "Striped White Sailor Tee",
-    color: "White",
-    gender: "Men",
-    image: "../images/tshirt4.png",
-    detailsPage: "StripedWhiteSailorTee.html",
-    description:
-      "Sail into style with our Striped White Sailor Tee! This classic striped shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a nautical touch to your wardrobe, it's a must-have for anyone who loves timeless fashion with a modern twist.",
-  },
-  {
-    name: "Tinky Winky Purple Tee",
-    color: "Purple",
-    gender: "Men",
-    image: "../images/tshirt5.png",
-    detailsPage: "TinkyWinkyPurpleTee.html",
-    description:
-      "Embrace your inner Tinky Winky with our Tinky Winky Purple Tee! This vibrant purple shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a playful pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.",
-  },
-  {
-    name: "Sunny Yellow Tee",
-    color: "Yellow",
-    gender: "Women",
-    image: "../images/tshirt6.png",
-    detailsPage: "SunnyYellowTee.html",
-    description:
-      "Brighten up your day with our Sunny Yellow Tee! This vibrant yellow shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a cheerful pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.",
-  },
-  {
-    name: "Capri-Sun Orange Tee",
-    color: "Orange",
-    gender: "Men",
-    image: "../images/tshirt7.png",
-    detailsPage: "CapriSunOrangeTee.html",
-    description:
-      "Dive into summer vibes with our Capri-Sun Orange Tee! This vibrant orange shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a sunny pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.",
-  },
-  {
-    name: "Trendy Teal Tee",
-    color: "Blue",
-    gender: "Men",
-    image: "../images/tshirt8.png",
-    detailsPage: "TrendyTealTee.html",
-    description:
-      "Make a splash with our Trendy Teal Tee! This vibrant teal shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a cool pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.",
-  },
-  {
-    name: "Earth Green Tee",
-    color: "Green",
-    gender: "Women",
-    image: "../images/tshirt9.png",
-    detailsPage: "EarthGreenTee.html",
-    description:
-      "Embrace nature with our Earth Green Tee! This vibrant green shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a natural pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.",
-  },
-  {
-    name: "Army Green Tee",
-    color: "Green",
-    gender: "Men",
-    image: "../images/tshirt10.png",
-    detailsPage: "ArmyGreenTee.html",
-    description:
-      "Go for a rugged look with our Army Green Tee! This versatile green shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a military-inspired touch to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.",
-  },
-  {
-    name: "Washed Black Tee",
-    color: "Black",
-    gender: "Men",
-    image: "../images/tshirt11.png",
-    detailsPage: "WashedBlackTee.html",
-    description:
-      "Add a vintage vibe to your wardrobe with our Washed Black Tee! This faded black shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a worn-in pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.",
-  },
-  {
-    name: "Off-White Tee",
-    color: "White",
-    gender: "Men",
-    image: "../images/tshirt12.png",
-    detailsPage: "OffWhiteTee.html",
-    description:
-      "Keep it classic with our Off-White Tee! This versatile off-white shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a neutral pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.",
-  },
-  {
-    name: "Washed Red Tee",
-    color: "Red",
-    gender: "Men",
-    image: "../images/tshirt13.png",
-    detailsPage: "WashedRedTee.html",
-    description:
-      "Add a vintage vibe to your wardrobe with our Washed Red Tee! This faded red shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a worn-in pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.",
-  },
-  {
-    name: "Turquoise Tee",
-    color: "Blue",
-    gender: "Women",
-    image: "../images/tshirt14.png",
-    detailsPage: "TurquoiseTee.html",
-    description:
-      "Make a splash with our Turquoise Tee! This vibrant turquoise shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a cool pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.",
-  },
-  {
-    name: "Spicy Purple Tee",
-    color: "Purple",
-    gender: "Women",
-    image: "../images/tshirt15.png",
-    detailsPage: "SpicyPurpleTee.html",
-    description:
-      "Turn up the heat with our Spicy Purple Tee! This vibrant purple shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a bold pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.",
-  },
-];
+// TODO: discuss changes -> products array exported to common JS file
 
 // fetch the productsRow div to insert the created cards from the array into the HTML
 const productsRow = document.getElementById("productsRow");
@@ -181,14 +45,16 @@ products.forEach((product) => {
   productsRow.innerHTML += `
     <div class="col-12 col-md-4 mb-3">
       <div class="card h-100">
-      <a href="${product.detailsPage}">
+      <a href="ProductDetail.html?id=${product.id}"> 
         <img class="card-img-top" src="${product.image}" alt="${product.name}" />
         </a>
         <div class="card-body">
           <h5 class="card-title">${product.name}</h5>
+          <p align=left>${product.price} EUR</p>
           <button class="btn btn-dark">Add to cart</button>
         </div>
       </div>
     </div>
   `;
 });
+// TODO: discuss changes in line 48 and 53
--- src/js/products.js
+++ src/js/products.js
@@ -1,33 +1,33 @@
-function tShirt(id, gender, color, price, pictureFileName, description) {
-    this.id = id;
-    this.gender = gender; // male or female
+function tShirt(name, color, gender, image, description, price, id) {
+    this.name = name;
     this.color = color;
-    this.price = price;
-    this.pictureFileName = pictureFileName;
+    this.gender = gender; // male or female
+    this.image = image;
     this.description = description;
+    this.price = price;
+    this.id = id;
 }
 
-// Array of t-shirt products
-const tShirts = [
-    new tShirt(1, "male", "black", 19.99, "tshirt-black-male.jpg", "Classic black cotton t-shirt with crew neck"),
-    new tShirt(2, "male", "white", 19.99, "tshirt-white-male.jpg", "Essential white t-shirt in premium cotton"),
-    new tShirt(3, "male", "navy", 22.99, "tshirt-navy-male.jpg", "Navy blue t-shirt with modern fit"),
-    new tShirt(4, "male", "grey", 21.99, "tshirt-grey-male.jpg", "Heather grey comfortable everyday tee"),
-    new tShirt(5, "male", "red", 24.99, "tshirt-red-male.jpg", "Bold red t-shirt for standout style"),
-    new tShirt(6, "male", "black", 23.99, "tshirt-black-male-2.jpg", "Premium black t-shirt with V-neck design"),
-    new tShirt(7, "male", "blue", 22.99, "tshirt-blue-male.jpg", "Sky blue relaxed fit t-shirt"),
-    new tShirt(8, "male", "white", 24.99, "tshirt-white-male-2.jpg", "Organic white cotton athletic tee"),
-    new tShirt(9, "male", "grey", 25.99, "tshirt-grey-male-2.jpg", "Charcoal grey premium blend tee"),
-    new tShirt(10, "male", "navy", 23.99, "tshirt-navy-male-2.jpg", "Navy blue long sleeve t-shirt"),
-    new tShirt(11, "female", "black", 19.99, "tshirt-black-female.jpg", "Classic black fitted t-shirt"),
-    new tShirt(12, "female", "white", 19.99, "tshirt-white-female.jpg", "Clean white essential tee"),
-    new tShirt(13, "female", "pink", 24.99, "tshirt-pink-female.jpg", "Soft pink v-neck t-shirt"),
-    new tShirt(14, "female", "black", 25.99, "tshirt-black-female-2.jpg", "Black scoop neck premium tee"),
-    new tShirt(15, "female", "grey", 23.99, "tshirt-grey-female.jpg", "Light grey minimalist tee"),
-    new tShirt(16, "female", "pink", 24.99, "tshirt-pink-female-2.jpg", "Rose pink crop style t-shirt"),
-    new tShirt(17, "female", "white", 21.99, "tshirt-white-female-2.jpg", "White oversized comfort tee"),
-    new tShirt(18, "female", "navy", 22.99, "tshirt-navy-female.jpg", "Navy blue versatile t-shirt"),
-    new tShirt(19, "female", "grey", 26.99, "tshirt-grey-female-2.jpg", "Heather grey fitted athletic tee"),
-    new tShirt(20, "female", "blue", 25.99, "tshirt-blue-female.jpg", "Sky blue casual everyday t-shirt")
+// replaced the object for a constructor to avoid errors with fields and reduce verbosity
+// there was an error in the first object due to this exact reason, also, no prices were set, which makes no sense, probably simply overlooked
+// added an id to generate the ProductDetail page off a single html document, but reading different ids
+
+const products = [
+    new tShirt("Classic Black Tee", "Black", "female", "../images/tshirt1.png", "A timeless black tee that pairs well with any outfit. Made from soft, breathable cotton for all-day comfort.", 24.99, 1),
+    new tShirt("Flamin' Hot Red Tee", "Red", "female", "../images/tshirt2.png", "Turn up the heat with our Flamin' Hot Red Tee! This vibrant red shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a bold pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.", 26.99, 2),
+    new tShirt("Ocean Blue Tee", "Blue", "female", "../images/tshirt3.png", "Dive into style with our Ocean Blue Tee! This vibrant blue shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a refreshing pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.", 22.99, 3),
+    new tShirt("Striped White Sailor Tee", "White", "male", "../images/tshirt4.png", "Sail into style with our Striped White Sailor Tee! This classic striped shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a nautical touch to your wardrobe, it's a must-have for anyone who loves timeless fashion with a modern twist.", 28.99, 4),
+    new tShirt("Tinky Winky Purple Tee", "Purple", "male", "../images/tshirt5.png", "Embrace your inner Tinky Winky with our Tinky Winky Purple Tee! This vibrant purple shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a playful pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.", 24.99, 5),
+    new tShirt("Sunny Yellow Tee", "Yellow", "female", "../images/tshirt6.png", "Brighten up your day with our Sunny Yellow Tee! This vibrant yellow shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a cheerful pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.", 22.99, 6),
+    new tShirt("Capri-Sun Orange Tee", "Orange", "male", "../images/tshirt7.png", "Dive into summer vibes with our Capri-Sun Orange Tee! This vibrant orange shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a sunny pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.", 26.99, 7),
+    new tShirt("Trendy Teal Tee", "Blue", "male", "../images/tshirt8.png", "Make a splash with our Trendy Teal Tee! This vibrant teal shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a cool pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.", 29.99, 8),
+    new tShirt("Earth Green Tee", "Green", "female", "../images/tshirt9.png", "Embrace nature with our Earth Green Tee! This vibrant green shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a natural pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.", 24.99, 9),
+    new tShirt("Army Green Tee", "Green", "male", "../images/tshirt10.png", "Go for a rugged look with our Army Green Tee! This versatile green shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a military-inspired touch to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.", 22.99, 10),
+    new tShirt("Washed Black Tee", "Black", "male", "../images/tshirt11.png", "Add a vintage vibe to your wardrobe with our Washed Black Tee! This faded black shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a worn-in pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.", 28.99, 11),
+    new tShirt("Off-White Tee", "White", "male", "../images/tshirt12.png", "Keep it classic with our Off-White Tee! This versatile off-white shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a neutral pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.", 20.00, 12),
+    new tShirt("Washed Red Tee", "Red", "male", "../images/tshirt13.png", "Add a vintage vibe to your wardrobe with our Washed Red Tee! This faded red shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a worn-in pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.", 26.99, 13),
+    new tShirt("Turquoise Tee", "Blue", "female", "../images/tshirt14.png", "Make a splash with our Turquoise Tee! This vibrant turquoise shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a cool pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.", 29.99, 14),
+    new tShirt("Spicy Purple Tee", "Purple", "female", "../images/tshirt15.png", "Turn up the heat with our Spicy Purple Tee! This vibrant purple shirt is made from soft, breathable cotton for all-day comfort. Perfect for adding a bold pop of color to your wardrobe, it's a must-have for anyone who loves to make a statement with their style.", 28.99, 15),
 ];
+
 
