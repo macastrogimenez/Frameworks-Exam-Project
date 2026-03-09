b80bd19bc8daf058d968dcaa6612fa4525fd2e12
macastrogimenez <migca@itu.dk>
2026-03-09 12:42:03 +0100

    added function to remove one product from a certain item ID from the basket and added some comments to BasketPage.js

--- src/js/BasketPage.js
+++ src/js/BasketPage.js
@@ -35,10 +35,24 @@ function increaseProductCounter(productId, basket) {
   saveBasketToLS(basket);
 }
 
+// function called by the 'Place an order' button -> it simply clears basketProducts from localStorage
 function placeOrder() {
   localStorage.removeItem("basketProducts");
 }
 
+// called by the removeButton next to every ite-> it retrieves basketProducts from localStorage, then reduces the number of products
+    // of a specific product id by one, saves basketProducts to localStorage once more and reloads the page.
+function removeFromBasket(prodId){
+  let basketArray = basketFromLStoArray(localStorage.getItem("basketProducts"));
+  basketArray[prodId] = basketArray[prodId] - 1;
+  saveBasketToLS(basketArray);
+  document.location.reload();
+}
+
+// It creates the table of products in the basket by retrieving the basketProducts array from localStorage
+  // then it iterates over every index and if the count > 0 it adds the product to the table with id, name,
+  // price per unit, quantity, total price and the possibility to remove an item. 
+  // Lastly, it calculates the total price of the basket
 function renderBasketPage() {
   const basket = document.getElementById("basket");
   if (!basket || typeof products === "undefined") return;
@@ -50,19 +64,20 @@ function renderBasketPage() {
   }
 
   basket.innerHTML += `
-  <table class="table">
-    <thead class="table-light">
-      <tr>
-        <th scope="col">Product Id<th/>
-        <th scope="col">Product Name<th/>
-        <th scope="col">Price Per Unit<th/>
-        <th scope="col">Quantity<th/>
-        <th scope="col">Total Price<th/>
-      </tr>
-    </thead>
-    <tbody id="basketBody">
-    </tbody>
-  </table>
+    <table class="table">
+      <thead class="table-light">
+        <tr>
+          <th scope="col">Product Id<th/>
+          <th scope="col">Product Name<th/>
+          <th scope="col">Price Per Unit<th/>
+          <th scope="col">Quantity<th/>
+          <th scope="col">Total Price<th/>
+          <th scope="col">Remove an item<th/>
+        </tr>
+      </thead>
+      <tbody id="basketBody">
+      </tbody>
+    </table>
   `;
 
   const basketBody = document.getElementById("basketBody");
@@ -76,13 +91,14 @@ function renderBasketPage() {
       const prod = products[i];
       total = Number(total + basketArray[i] * prod.price);
       basketBody.innerHTML += `
-      <tr>
-        <td>#${i}<td/>
-        <td>${prod.name}<td/>
-        <td>${prod.price} EUR<td/>
-        <td>${basketArray[i]}<td/>
-        <td>${(basketArray[i] * prod.price).toFixed(2)} EUR<td/>
-      </tr>
+        <tr>
+          <td>#${i}<td/>
+          <td>${prod.name}<td/>
+          <td>${prod.price} EUR<td/>
+          <td>${basketArray[i]}<td/>
+          <td>${(basketArray[i] * prod.price).toFixed(2)} EUR<td/>
+          <td><button id="removeButton" class="btn btn-dark" onclick="removeFromBasket(${i})">-</button></td>
+        </tr>
     `;
     }
   }
