f9f16e0ca05902f2016d3677118411791d545961
Sine Ergin <sine-ergin@live.dk>
2026-03-30 23:45:08 +0200

    create POST endpoint to  Create a basket for one specific user based on username: basketRouter.post("/user/:username/basket", createBasket);

--- src/server/API/users/basket.route.js
+++ src/server/API/users/basket.route.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { getBasket } from "./basket.controller.js";
+import { createBasket, getBasket } from "./basket.controller.js";
 export const basketRouter = express.Router();
 // The controller handles request/response logic.
 //The model reads data from products.json.
 
 // API endpoints for products
 
+// POST: Createbasket for one specific user based on username
+basketRouter.post("/user/:username/basket", createBasket);
+
 //Get basket for a certain user
 basketRouter.get("/basket/:username", getBasket);
-
