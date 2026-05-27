497037035d19cc1873724612e006d2ad2cfb3a13
melpuck <puckmelanie@gmail.com>
2026-05-27 17:41:35 +0200

    Aligning design

--- client/src/components/navbar/NavigationBar.tsx
+++ client/src/components/navbar/NavigationBar.tsx
@@ -3,17 +3,18 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
+import "./NavigationBar.css";
 
 function NavigationBar() {
     const { user, isLoggedIn, logout } = useAuth();
 
     return (
-        <Navbar expand="lg" bg="body-tertiary" className="px-3">
+        <Navbar expand="lg" className="site-header px-3">
             <Container fluid>
                 <div className="d-flex align-items-center gap-2">
                     <Navbar.Brand as={Link} to="/" className="mb-0">
                         <img
-                            alt=""
+                            alt="Brand Logo"
                             src="/images/brandlogo.png"
                             width="100"
                             height="100"
@@ -22,7 +23,7 @@ function NavigationBar() {
                     </Navbar.Brand>
 
                     {isLoggedIn && (
-                        <Navbar.Text className="mb-0">
+                        <Navbar.Text className="mb-0 subheading">
                             Hi again, {user?.firstName}.
                         </Navbar.Text>
                     )}
@@ -37,7 +38,7 @@ function NavigationBar() {
                         <Nav.Link as={Link} to="/basket">Basket</Nav.Link>
 
                         {isLoggedIn ? (
-                            <button onClick={logout} className="btn btn-outline-dark">
+                            <button onClick={logout} className="btn btn-outline-dark btn-small btn-rounded-none">
                                 Logout
                             </button>
                         ) : (
