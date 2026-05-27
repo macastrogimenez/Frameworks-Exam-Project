08f4bedc4a9a7765a16c06dd1cf9a023b96ea404
macastrogimenez <migca@itu.dk>
2026-05-27 12:10:50 +0200

    fix: update user greeting display in NavigationBar and add new user entry in users.json

--- client/src/components/navbar/NavigationBar.tsx
+++ client/src/components/navbar/NavigationBar.tsx
@@ -10,38 +10,40 @@ function NavigationBar() {
     return (
         <Navbar expand="lg" bg="body-tertiary" className="px-3">
             <Container fluid>
-                <Navbar.Brand as={Link} to="/">
-                    <img
-                        alt=""
-                        src="/images/brandlogo.png"
-                        width="100"
-                        height="100"
-                        className="d-inline-block align-center"
-                    />{" "}
-                </Navbar.Brand>
+                <div className="d-flex align-items-center gap-2">
+                    <Navbar.Brand as={Link} to="/" className="mb-0">
+                        <img
+                            alt=""
+                            src="/images/brandlogo.png"
+                            width="100"
+                            height="100"
+                            className="d-inline-block align-center"
+                        />
+                    </Navbar.Brand>
+
+                    {isLoggedIn && (
+                        <Navbar.Text className="mb-0">
+                            Hi again, {user?.firstName}.
+                        </Navbar.Text>
+                    )}
+                </div>
 
                 <Navbar.Toggle aria-controls="main-navbar" />
 
                 <Navbar.Collapse id="main-navbar">
-                    <div className="d-flex flex-column flex-lg-row align-items-lg-center w-100 gap-3">
-                        {isLoggedIn && (
-                            <span className="navbar-text">Hi again, {user?.firstName}.</span>
+                    <Nav className="ms-lg-auto align-items-lg-center gap-3">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/products">All Products</Nav.Link>
+                        <Nav.Link as={Link} to="/basket">Basket</Nav.Link>
+
+                        {isLoggedIn ? (
+                            <button onClick={logout} className="btn btn-outline-dark">
+                                Logout
+                            </button>
+                        ) : (
+                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
                         )}
-
-                        <Nav className="ms-lg-auto align-items-lg-center gap-3">
-                            <Nav.Link as={Link} to="/">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/products">All Products</Nav.Link>
-                            <Nav.Link as={Link} to="/basket">Basket</Nav.Link>
-
-                            {isLoggedIn ? (
-                                <button onClick={logout} className="btn btn-outline-dark">
-                                    Logout
-                                </button>
-                            ) : (
-                                <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                            )}
-                        </Nav>
-                    </div>
+                    </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
