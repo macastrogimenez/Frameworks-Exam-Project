01f829686dec035f43d5b6993495f96e6a7d3a04
ceciliemalmberg <cecilie.malmberg@awaze.com>
2026-05-28 17:17:44 +0200

    deleting unused endpoint

--- server/API/basket/basket.controller.js
+++ server/API/basket/basket.controller.js
@@ -1,33 +1,5 @@
 import * as basketModel from "./basket.model.js";
 
-// Post a basket for a user based on username.
-export async function createBasket(req, res) {
-  try {
-    const username = req.params.username;
-    const basket = req.body.basket;
-
-    // Calling model function to create basket for the user.
-    const createBasketforUser = await basketModel.createBasketForUser(
-      username,
-      basket,
-    );
-
-    // model returns null if the user doesn't exist
-    if (createBasketforUser === null) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    // model returns "already_exists" if the user already has a basket.
-    if (createBasketforUser === "already_exists") {
-      return res.status(409).json({ error: "Basket already exists" });
-    }
-
-    return res.status(201).json(createBasketforUser);
-  } catch (error) {
-    return res.status(400).json({ error: error.message });
-  }
-}
-
 //Put a product in a specific user's basket\
 export async function updateBasket(req, res) {
   try {
